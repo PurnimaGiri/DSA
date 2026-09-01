e63f4b9610d64552653c83eecdcc6e2a8d563372
PurnimaGiri <purnimagiri156@gmail.com>
2026-09-01 10:45:36 +0530

    Implement triplet sum check in array

---
+++ triplet_sum_in_arr_gfg.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} arr
+ * @param {number} target
+ * @returns {boolean}
+ */
+
+class Solution {
+    hasTripletSum(arr, target) {
+        for(let i = 0 ; i < arr.length - 2 ; i++){
+            for(let j = i + 1 ; j < arr.length  - 1 ; j++){
+                for(let k = j + 1; k < arr.length ; k++){
+                    if(arr[i] + arr[j] + arr[k] == target){
+                        return true
+                        break
+                    }
+                    
+                }
+            }
+        }
+        return false
+        
+    }
+    
+}
