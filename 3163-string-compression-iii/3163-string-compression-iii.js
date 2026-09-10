d2f00d09ac46c5875dfaa906b7b877e7a3402dde
PurnimaGiri <purnimagiri156@gmail.com>
2026-09-10 16:48:43 +0530

    Time: 26 ms (72.22%), Space: 68.1 MB (88.89%) - LeetHub

---
+++ 3163-string-compression-iii/3163-string-compression-iii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} word
+ * @return {string}
+ */
+var compressedString = function(word) {
+    let comp = "";
+    let i = 0;
+    while (i < word.length) {
+        let ch = word[i];
+        let count = 0;
+        while (i < word.length && word[i] === ch && count < 9) {
+            count++;
+            i++;
+        }
+        comp += count + ch;
+    }
+
+    return comp;
+};
